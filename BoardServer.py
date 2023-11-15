ec092786b3aca362ec20d77a27e5e8ab7b192ec8
topojs8 <topojs@gmail.com>
2023-11-16 01:54:10 +0900

    ENH: 성능테스트 시나리오 작성 및 스크립트 작성

--- BoardServer.py
+++ BoardServer.py
@@ -1,17 +1,16 @@
 from locust import HttpUser, task, between
+from random import random
 
 class BoardServer(HttpUser):
     wait_time = between(1, 2)
 
     def on_start(self):
-        self.client.post("/login", json={"username":"foo", "password":"bar"})
-
-    @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
-
+        self.client.post("/users/sign-in", json={"userId": "topojs9",
+                                                 "password": "123"})
     @task(3)
     def view_item(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
+        sortStatus = random.choice(["CATEGORIES", "NEWEST", "OLDEST", "HIGHPRICE", "LOWPRICE", "GRADE"])
+        name = ''.join(random.choice('AaBb') for _ in range(1))
+        self.client.get(f"/search?name={name}&sortStatus={sortStatus}", name="/item")
+
+
