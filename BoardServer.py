68f36d5927ca2973e2d7f65e44ff60316d69ff38
topojs8 <topojs@gmail.com>
2023-11-20 23:44:39 +0900

    ENH: BoardServer 검색 api 연동 시나리오 작성

--- BoardServer.py
+++ BoardServer.py
@@ -1,5 +1,8 @@
+import json
+
 from locust import HttpUser, task, between
-from random import random
+import random
+
 
 class BoardServer(HttpUser):
     wait_time = between(1, 2)
@@ -7,10 +10,15 @@ class BoardServer(HttpUser):
     def on_start(self):
         self.client.post("/users/sign-in", json={"userId": "topojs9",
                                                  "password": "123"})
+
     @task(3)
     def view_item(self):
         sortStatus = random.choice(["CATEGORIES", "NEWEST", "OLDEST", "HIGHPRICE", "LOWPRICE", "GRADE"])
-        name = ''.join(random.choice('AaBb') for _ in range(1))
-        self.client.get(f"/search?name={name}&sortStatus={sortStatus}", name="/item")
-
-
+        categoryId = random.randint(1, 10)
+        name = '테스트 게시글'.join(str(random.randint(1, 10000)))
+        headers = {'Content-Type': 'application/json'}
+        data = {"sortStatus": sortStatus,
+                "categoryId": categoryId,
+                "name": name}
+        # print(data)
+        self.client.post("/search", json=data, headers=headers)
